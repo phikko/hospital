c3eecbf8e96f89a1547c6f79c5b2a4bfd4dd6ba7
phikko <skeezze@gmail.com>
2021-01-07 18:58:53 +0100

    Added user creation form

--- Actions/Actions/Users/UsersCreate.cs
+++ Actions/Actions/Users/UsersCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Hospital.Actions.Classes;
+using Hospital.Database;
 using Hospital.Database.Models;
 
 namespace Hospital.Actions.Actions.Users
@@ -10,8 +11,9 @@ namespace Hospital.Actions.Actions.Users
         {
             Console.Clear();
             Console.WriteLine("CREATE NEW USER");
+
             User user = new User();
-            
+
             Console.Write("First name: ");
             user.Firstname = Console.ReadLine();
             Console.Write("Last name: ");
@@ -23,16 +25,41 @@ namespace Hospital.Actions.Actions.Users
             Console.Write("Password: ");
             user.Password = Console.ReadLine();
 
-            try
+            Console.Write("USER TYPE");
+            Console.Write("1. Admin");
+            Console.Write("2. Nurse");
+            Console.Write("3. Doctor");
+            int input = 0;
+            User userFromDb = null;
+            while (!int.TryParse(Console.ReadLine(), out input))
             {
-                new UserModel().CreateUser(user);
-                Console.Clear();
-                Console.WriteLine("New user has been added to the database.");
+                Console.WriteLine("Wrong Input!");
             }
-            catch (Exception e)
+            switch (input)
             {
-                Console.WriteLine("Oops! Something went wrong while creating a user!");
+                case 1:
+                    userFromDb = new UserModel().CreateUser(user);
+                    new AdminModel().CreateAdmin(new Admin {Id = userFromDb.Id});
+                    break;
+                case 2:
+                    userFromDb = new UserModel().CreateUser(user);
+                    new NurseModel().CreateNurse(new Nurse {Id = userFromDb.Id});
+                    break;
+                case 3:
+                    Doctor doctor = new Doctor();
+                    Console.Write("Speciality: ");
+                    doctor.Speciality = Console.ReadLine();
+                    Console.Write("PWZ: ");
+                    doctor.Pwz = Console.ReadLine();
+                    userFromDb = new UserModel().CreateUser(user);
+                    doctor.UserId = userFromDb.Id;
+                    new DoctorModel().CreateDoctor(doctor);
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input!");
+                    break;
             }
+            Console.WriteLine("User succesfully added!");
         }
     }
 }
